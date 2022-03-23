d0b7a62108fbd2a5f5eda9145c6fe3d3e95cae50
Miguel Ortega <miguelorab@gmail.com>
2022-03-23 12:17:30 +0000

    Prueba grados.js

---
+++ js/grados.js
@@ -0,0 +1,37 @@
+$(document).ready(function (){
+  $.getJSON( "ajax/test.json", function( data ) {
+    var items = [];
+    $.each( data, function( id, img, name, description, subject, href) {
+      items.push( "<ul class=" + "listaGrados" + ">" );
+      items.push( " <div class=" + "fotoinfogrado" + ">" );
+      items.push( "   <li class=" + "fotoGrado" + "><a><img class=" + "imgGrado" + "src=" + img + " alt=" + "menu" + "width=" + "120" + " height=" + "120" + "></a></li>" );
+      items.push( " </div>" );
+      items.push( " <div class=" + "cajaInfoGrados" + ">" );
+      items.push( "  <li><h2>" + name +  "</h2></li>" );
+      items.push( "  <li class=" + "descripcion" + "><p>" + description + "</p></li>" )
+      items.push( "  <li><a href=" + "asignaturasgrado.html" + "> Más información </a></li>" );
+      items.push( " </div>" );
+      items.push( "</ul>" );
+    });
+
+    $( "<ul/>", {
+      "class": "listaGrados",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
+});
+
+// <ul class="listaGrados">
+//  <div class="fotoinfogrado">
+//    <li class="fotoGrado"><a><img class="imgGrado" src= + "" +  alt="menu" width="120" height="120"></a></li>
+//  </div>
+//  <div class="cajaInfoGrados">
+//    <li><h2> + "" +  </h2></li>
+//    <li class="descripcion"><p> + "" + </p></li>
+//    <li><a href="asignaturasgrado.html"> Más información </a></li>
+//  </div>
+// </ul>
+
+
+
+
---
+++ js/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function (){
+  $('#login').submit(function(evento)){
+    evento.preventDefault();
+    var dni = $('#textdni').val();
+    var contraseña = $('#textocontraseña').val();
+    var contraseñaR = $('#textocontraseñaR').val();
+
+  }
+});

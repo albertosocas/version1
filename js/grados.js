5faddf404f320278441d19df27a0996cb00c2919
alber <albertitosocas@hotmail.com>
2022-03-23 12:03:11 +0000

    json version 2

--- js/grados.js
+++ js/grados.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-
-  var padreSuperior=$(this).parent().parent();
-  $(".botonFoto").click(function(){
-    $(".listaGrados").hide();
-    $(padreSuperior).show();
-  });
-
+  var json = $.getJSON("scripts/grados.json");
+  
 });

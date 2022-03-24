198554500a70f7244a62ed55850a2b28a672b0b0
David Barcon <baroconiebladavid@gmail.com>
2022-03-24 19:03:27 +0000

    codigo mas limpio facilitado el acceso al json

--- js/asignaturas.js
+++ js/asignaturas.js
@@ -9,40 +9,29 @@ $(document).ready(function (){
 
 
   $.getJSON( "scripts/asignatura.json", function( data ) {
-
     var items = [];
-    var items2 = [];
 
     $.each( data, function() {
-      $.each( this, function(key, val){
-        items.push(val);
-      });
 
-      var id = items[0];
-      var img = items[1];
-      var name = items[2];
-      var description = items[3];
-      var grados = items[4];
-      var href = items[5];
-      if(asigs.includes(id)) {
-        items2.push("<ul class=" + "listaAsignaturas" + ">");
-        items2.push(" <div class=" + "fotoinfoasignatura" + ">");
-        items2.push("   <li class=" + "fotoAsignatura" + "><a><img class=" + "imgAsignatura" + " src=" + img + " alt=" + "menu" + " width=" + "120" + " height=" + "120" + "></a></li>");
-        items2.push(" </div>");
-        items2.push(" <div class=" + "cajaInfoAsignaturas" + ">");
-        items2.push("  <li><h2>" + name + "</h2></li>");
-        items2.push("  <li class=" + "descripcion" + "><p>" + description + "</p></li>")
-        items2.push("  <li><a href=" + "asignatura.html?idvalue=" + id + "> Más información </a></li>");
-        items2.push(" </div>");
-        items2.push("</ul>");
+      if(asigs.includes(this["id"])) {
+        items.push("<ul class=" + "listaAsignaturas" + ">");
+        items.push(" <div class=" + "fotoinfoasignatura" + ">");
+        items.push("   <li class=" + "fotoAsignatura" + "><a><img class=" + "imgAsignatura" + " src=" + this["img"] + " alt=" + "menu" + " width=" + "120" + " height=" + "120" + "></a></li>");
+        items.push(" </div>");
+        items.push(" <div class=" + "cajaInfoAsignaturas" + ">");
+        items.push("  <li><h2>" + this["name"] + "</h2></li>");
+        items.push("  <li class=" + "descripcion" + "><p>" + this["description"] + "</p></li>")
+        items.push("  <li><a href=" + "asignatura.html?idvalue=" + this["id"] + "> Más información </a></li>");
+        items.push(" </div>");
+        items.push("</ul>");
       $( "<ul/>", {
         "class": "listaAsignaturas",
-        html: items2.join( "" )
+        html: items.join( "" )
       }).appendTo( "main" );
       };
 
       items = [];
-      items2 = [];
+      items = [];
     });
 
 
--- js/grados.js
+++ js/grados.js
@@ -1,29 +1,20 @@
 $(document).ready(function (){
   $.getJSON( "scripts/grados.json", function( data ) {
     var items = [];
-    var items2 = [];
 
     $.each( data, function() {
-      $.each( this, function(key, val){
-        items.push(val);
-      });
 
-      var id = items[0];
-      var img = items[1];
-      var name = items[2];
-      var description = items[3];
-      var subject = items[4];
-      var href = items[5];
-      items2.push( "<ul class=" + "listaGrados" + ">" );
-      items2.push( " <div class=" + "fotoinfogrado" + ">" );
-      items2.push( "   <li class=" + "fotoGrado" + "><a><img class=" + "imgGrado" + " src=" + img + " alt=" + "menu" + " width=" + "120" + " height=" + "120" + "></a></li>" );
-      items2.push( " </div>" );
-      items2.push( " <div class=" + "cajaInfoGrados" + ">" );
-      items2.push( "  <li><h2>" + name +  "</h2></li>" );
-      items2.push( "  <li class=" + "descripcion" + "><p>" + description + "</p></li>" )
-      items2.push( "  <li><a href=" + "asignaturasgrado.html?asig=" + subject + "> Más información </a></li>" );
-      items2.push( " </div>" );
-      items2.push( "</ul>" );
+      items.push( "<ul class=" + "listaGrados" + ">" );
+      items.push( " <div class=" + "fotoinfogrado" + ">" );
+      items.push( "   <li class=" + "fotoGrado" + "><a><img class=" + "imgGrado" + " src=" + this["img"] + " alt=" + "menu" + " width=" + "120" + " height=" + "120" + "></a></li>" );
+      items.push( " </div>" );
+      items.push( " <div class=" + "cajaInfoGrados" + ">" );
+      items.push( "  <li><h2>" + this["name"] +  "</h2></li>" );
+      items.push( "  <li class=" + "descripcion" + "><p>" + this["description"] + "</p></li>" )
+      items.push( "  <li><a href=" + "asignaturasgrado.html?asig=" + this["asignaturas"] + "> Más información </a></li>" );
+      items.push( " </div>" );
+      items.push( "</ul>" );
+
 
 
 
@@ -31,11 +22,11 @@ $(document).ready(function (){
 
       $( "<ul/>", {
         "class": "listaGrados",
-        html: items2.join( "" )
+        html: items.join( "" )
       }).appendTo( "main" );
 
       items = [];
-      items2 = [];
+      items = [];
     });
 
   });
